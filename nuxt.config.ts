2901b2d4fef010f9f7685c0cde497b132c07e09d
Marcos <65628817+Marcos-MEDENOU@users.noreply.github.com>
2026-04-03 01:28:05 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,8 +34,8 @@ export default defineNuxtConfig({
   },
    runtimeConfig: {
     public: {
-      frontUrl: process.env.APP_URL || "https://africstoryline.shop",
-      apiBackendUrl: process.env.SERVER_URL || "https://api.africstoryline.shop",
+      frontUrl: process.env.APP_URL || "https://fast-resto.shop",
+      apiBackendUrl: process.env.SERVER_URL || "https://api.fast-resto.shop",
     }
   },
 })
