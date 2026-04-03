7e66f9a8c61a89c043e9e2f10534329a19a3d36f
Kola77pic04 <kolaopportunity@gmail.com>
2026-04-03 16:54:15 +0200

    maj

--- app/pages/discover.vue
+++ app/pages/discover.vue
@@ -20,7 +20,7 @@
             </div>
 
             <!-- FEED -->
-            <TransitionGroup name="feed" tag="div" v-if="feed.length > 2">
+            <TransitionGroup name="feed" tag="div" v-if="loading && feed.length > 2">
                 <div v-for="(item, index) in feed"
                     :key="item.type === 'category' ? `category-${index}` : `${item.type}-${item.data.uuid}`"
                     class="absolute inset-0 transition-all duration-500 flex items-end justify-center" :class="{
@@ -185,7 +185,7 @@
                                         {{ getInitials(item.data.followers[item.data.followers.length - 1]?.name) }}
                                     </span>
                                     <span class="text-xs">{{ item.data.followers[item.data.followers.length - 1]?.name
-                                        }} le suit</span>
+                                    }} le suit</span>
                                 </div>
                             </div>
 
@@ -214,7 +214,7 @@
             </TransitionGroup>
 
             <!-- EMPTY STATE -->
-            <div v-else
+            <div v-if="loading && feed.length < 2"
                 class="absolute inset-0 flex flex-col items-center justify-center text-center px-6 z-10">
 
                 <div class="flex flex-col items-center gap-4">
@@ -246,13 +246,31 @@
             </div>
 
             <!-- 🖥️ NAV -->
-            <div class="hidden md:flex absolute right-4 top-1/2 flex-col gap-3 z-20" v-if="feed.length > 2">
+            <div class="hidden md:flex absolute right-4 top-1/2 flex-col gap-3 z-20" v-if="loading && feed.length > 2">
                 <button @click="prev" :class="prevClass ? 'bg-white text-slate-900' : 'bg-white/50'" class="nav-btn">
                     <Icon name="mdi:arrow-top" class="w-5 h-5" />
                 </button>
-                <button @click="next" :class="nextClass ? 'bg-white text-slate-900' : 'bg-white/50'" class="nav-btn" v-if="currentIndex !== feed.length - 1">
+                <button @click="next" :class="nextClass ? 'bg-white text-slate-900' : 'bg-white/50'" class="nav-btn"
+                    v-if="currentIndex !== feed.length - 1">
                     <Icon name="mdi:arrow-bottom" class="w-5 h-5" />
                 </button>
+            </div>
+
+            <div v-if="!loading" class="absolute inset-0 flex items-center justify-center">
+
+                <div role="status">
+                    <svg aria-hidden="true" class="w-12 h-12 animate-spin fill-orange-600"
+                        viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor" />
+                        <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill" />
+                    </svg>
+                    <span class="sr-only">Loading...</span>
+                </div>
+
             </div>
         </div>
 
@@ -506,6 +524,7 @@ const currentIndex = ref(0);
 const showTimeHeart = ref<boolean>(false);
 const prevClass = ref<boolean>(false);
 const nextClass = ref<boolean>(false);
+const loading = ref<boolean>(false);
 const bg = ref<any>(["bg-purple-500", "bg-pink-500", "bg-sky-500", "bg-yellow-500", "bg-cyan-500", "bg-[#f50]", "bg-slate-500", "bg-teal-500", "bg-green-500", "bg-red-500"]);
 let startY = 0;
 let startX = 0;
@@ -827,6 +846,7 @@ const fetchFeed = async () => {
         feed.value.push(...filtered);
         page.value++;
     }
+    loading.value = true;
 };
 
 const handleKeydown = (e: KeyboardEvent) => {
