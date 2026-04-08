5d218e025a2b868710f43c9f9f3cf69e6b62e298
Kola77pic04 <kolaopportunity@gmail.com>
2026-04-08 10:34:34 +0200

    maj

--- app/components/Home/Catalog.vue
+++ app/components/Home/Catalog.vue
@@ -2,19 +2,50 @@
   <div id="popular">
     <section class="border-b border-slate-200 bg-white dark:bg-dark sticky top-14 lg:top-16 z-10 shadow-sm">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-slate-600 dark:text-primary">
-        <div class="flex items-center gap-2 py-4 overflow-x-auto custom">
-          <button
-            class="flex-shrink-0 px-4 py-1.5 rounded bg-slate-900 text-white text-xs font-medium transition-transform hover:scale-105 dark:border-gray-600 dark:border-[1px]">Pour
-            vous</button>
-          <div class="flex items-center gap-2" v-if="!loadingCategory && categories.length > 0">
-            <nuxt-link v-for="(category, index) in categories" :key="index" :to="`/categories/${category.uuid}`"
-              class="flex-shrink-0 px-4 lg:px-3 py-1.5 rounded bg-white border border-slate-200 hover:border-slate-300 hover:bg-slate-50 text-xs font-medium transition-all whitespace-nowrap dark:hover:bg-orange-50 dark:hover:border-orange-100/50 dark:hover:text-orange-800 dark:bg-transparent dark:text-slate-200 flex items-center justify-center gap-2">
-              <img :src="category.image?.includes('https') ? category.image : `${$config.public.apiBackendUrl}/uploads/categories/${category.image}`" :alt="category.name" class="w-5 h-5 rounded"> {{category.name }}</nuxt-link>
+
+        <div class="relative">
+
+          <!-- bouton gauche -->
+          <button @click="scrollLeft"
+            class="hidden lg:flex lg:justify-center lg:items-center w-9 h-9 absolute left-0 top-1/2 -translate-y-1/2 z-20 bg-slate-800 hover:bg-slate-900 hover:duration-300 hover:ease-linear text-white shadow p-2 rounded-full">
+            <Icon name="mdi:arrow-left" class="w-4 h-4" />
+          </button>
+
+          <!-- slider -->
+          <div ref="slider" class="flex items-center gap-2 py-4 overflow-x-auto no-scrollbar scroll-smooth">
+            <!-- bouton fixe -->
+            <button
+              class="flex-shrink-0 px-4 py-1.5 rounded bg-slate-900 text-white text-xs font-medium transition-transform hover:scale-105">
+              Pour vous
+            </button>
+
+            <!-- catégories -->
+            <div v-if="!loadingCategory && categories.length > 0" class="flex items-center gap-2">
+              <nuxt-link v-for="(category, index) in categories" :key="index" :to="`/categories/${category.uuid}`"
+                class="flex-shrink-0 px-4 lg:px-3 py-1.5 rounded bg-white dark:bg-transparent dark:text-slate-200 border border-slate-200 hover:border-slate-300 hover:bg-slate-50 text-xs font-medium whitespace-nowrap flex items-center gap-2">
+                <img :src="category.image?.includes('https')
+                  ? category.image
+                  : `${$config.public.apiBackendUrl}/uploads/categories/${category.image}`" :alt="category.name"
+                  class="w-5 h-5 rounded" />
+                {{ category.name }}
+              </nuxt-link>
+            </div>
+
+            <!-- skeleton -->
+            <div v-else class="flex items-center gap-2">
+              <button v-for="index in 8" :key="index"
+                class="animate-pulse flex-shrink-0 px-4 py-3.5 w-1/4 lg:w-1/3 rounded bg-gradient-to-r from-slate-200 via-slate-300 to-slate-200"></button>
+            </div>
           </div>
-          <div class="flex items-center gap-2" v-else>
-            <button v-for="index in 8" :key="index" class="animate-pulse flex-shrink-0 px-4 py-3.5 w-1/4 lg:w-1/3 rounded bg-white border border-slate-200 hover:border-slate-300 hover:bg-slate-50 text-xs font-medium transition-all whitespace-nowrap bg-gradient-to-r from-slate-200 via-slate-300 to-slate-200 dark:from-slate-700 dark:via-slate-600 dark:to-slate-700 bg-[length:200%_100%] animate-[shimmer_1.5s_infinite]"></button>
-          </div>
+
+          <!-- bouton droite -->
+          <button @click="scrollRight"
+            class="hidden lg:flex lg:justify-center lg:items-center w-9 h-9 absolute right-0 top-1/2 -translate-y-1/2 z-20 bg-slate-800 hover:bg-slate-900 hover:duration-300 hover:ease-linear text-white shadow p-2 rounded-full">
+            <Icon name="mdi:arrow-right" class="w-4 h-4" />
+          </button>
+
         </div>
+
       </div>
     </section>
 
@@ -73,6 +104,15 @@
   scrollbar-width: thin;
   scrollbar-color: #cbd5e1 transparent;
 }
+
+.no-scrollbar::-webkit-scrollbar {
+  display: none;
+}
+
+.no-scrollbar {
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
 </style>
 
 <script lang="ts" setup>
@@ -82,6 +122,26 @@ const loading = ref<boolean>(true);
 const loadingCategory = ref<boolean>(true);
 const categories = ref<Category[]>([]);
 const books = ref<BookData[]>([]);
+const slider = ref<HTMLElement | null>(null)
+
+const scrollLeft = () => {
+  if (!slider.value) return
+
+  slider.value.scrollBy({
+    left: -300,
+    behavior: "smooth"
+  })
+}
+
+const scrollRight = () => {
+  if (!slider.value) return
+
+  slider.value.scrollBy({
+    left: 300,
+    behavior: "smooth"
+  })
+}
+
 onMounted(async () => {
   categories.value = await allCategorieActifs();
   books.value = await getFiveTopBooks();
