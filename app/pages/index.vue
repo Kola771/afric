b21e013dc2392211e0a06dde8dd86daf5bb5e169
Kola771 <koladeaboudou@gmail.com>
2026-05-02 15:49:09 +0100

    maj faaaa

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,23 +8,23 @@
 </template>
 <script lang="ts" setup>
 const config = useRuntimeConfig();
-  useSeoMeta({
-    title: 'Accueil',
-    description: 'Explorez des histoires africaines originales écrites par des auteurs émergents et passionnés sur Afric Storyline.',
-    
-    ogTitle: 'Accueil',
-    ogDescription: 'Plongez dans des récits africains captivants publiés par une nouvelle génération d’auteurs talentueux.',
-    ogImage: `${config.public.frontUrl}/afric.png`,
-    ogUrl: `${config.public.frontUrl}`,
-    ogType: 'website',
+useSeoMeta({
+  title: 'Accueil',
+  description: 'Explorez des histoires africaines originales écrites par des auteurs émergents et passionnés sur Afric Storyline.',
 
-    twitterCard: 'summary_large_image',
-    twitterTitle: 'Accueil',
-    twitterDescription: 'Découvrez des récits africains uniques écrits par des auteurs émergents.',
-    twitterImage: `${config.public.frontUrl}/afric.png`
-  });
+  ogTitle: 'Accueil',
+  ogDescription: 'Plongez dans des récits africains captivants publiés par une nouvelle génération d’auteurs talentueux.',
+  ogImage: `${config.public.frontUrl}/afric.png`,
+  ogUrl: `${config.public.frontUrl}`,
+  ogType: 'website',
 
-  onMounted(() => {
+  twitterCard: 'summary_large_image',
+  twitterTitle: 'Accueil',
+  twitterDescription: 'Découvrez des récits africains uniques écrits par des auteurs émergents.',
+  twitterImage: `${config.public.frontUrl}/afric.png`
+});
+
+onMounted(() => {
   const LAST_PLAY_KEY = "last_audio_play";
   const ONE_WEEK = 7 * 24 * 60 * 60 * 1000;
 
@@ -33,12 +33,13 @@ const config = useRuntimeConfig();
 
   if (!lastPlay || now - parseInt(lastPlay) > ONE_WEEK) {
     const audio = new Audio("/assets/audios/faaaa.mp3"); // ton fichier ici
-
-    audio.play().then(() => {
-      localStorage.setItem(LAST_PLAY_KEY, now.toString());
-    }).catch(() => {
-      console.log("Lecture bloquée par le navigateur");
-    });
+    window.addEventListener("click", () => {
+      audio.play().then(() => {
+        localStorage.setItem(LAST_PLAY_KEY, now.toString());
+      }).catch(() => {
+        console.log("Lecture bloquée par le navigateur");
+      });
+    }, { once: true });
   }
 
   localStorage.removeItem("register_author");
